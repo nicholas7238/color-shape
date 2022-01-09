cac62446b5279ee53bc9f0b121a808fa03af20a7
nicholct <67173781+nicholct@users.noreply.github.com>
2022-01-09 13:37:34 -0800

    added a triforce

---
+++ src/Triforce.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Triforce.css';
+
+export default function Triforce() {
+
+    return (
+        <div>
+            <h2 className='DisplayName'>Nick</h2>
+            <div className='Triangle TopTri'></div>
+            <div>
+                <div className='Triangle BotTri'></div>
+                <div className='Triangle BotTri'></div>
+            </div>
+            
+            
+        </div>
+    )
+
+}
+
